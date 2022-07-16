5b593ca9861b562cbb6a5204075bb922cea91db7
srinidevlearn <fordevintend@gmail.com>
2022-07-16 21:02:34 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -3,4 +3,11 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Numbers</h1>`;
+
+let num1 = 1;
+// num1 = 'sa'; // throws error due to implicit type assignment
+
+let num2: number = 1;
+num2 = 3;
+console.log(num2);
