ec328e0854c40cdb329898e6b47b217f5fa9b45d
srinidevlearn <fordevintend@gmail.com>
2022-07-16 21:03:09 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Numbers</h1>`;
 
 let num1 = 1;
-// num1 = 'sa'; // throws error due to implicit type assignment
+num1 = 'sa'; // throws error due to implicit type assignment
 
 let num2: number = 1;
 num2 = 3;
-console.log(num2);
+console.log(num2, num1);
